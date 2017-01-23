f0d1d62aeafa832fef6586ec2f5554bf6a4512b2
fonny222 <christopherdfontana@gmail.com>
2017-01-22 21:34:08 -0600

    Ex-2 Java 2 SOS application
    Created an application to display medical info to emergency crews

---
+++ app/src/main/java/christopher_fontana/ex_2_sosapp_christopherfontana/MainActivity.java
@@ -0,0 +1,13 @@
+package christopher_fontana.ex_2_sosapp_christopherfontana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
